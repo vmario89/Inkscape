6562be0fbad0760cb53fdc55a3ef4140ce5bbeb9
Mario Voigt <opensource@leyghis.de>
2020-08-15 12:46:06 +0200

    Update Circle.py

--- geometry/Circle.py
+++ geometry/Circle.py
@@ -1,23 +1,27 @@
-#!/usr/bin/env python
-#coding: UTF-8
-import sys,os,math
-sys.path.append("/Applications/Inkscape.app/Contents/Resources/extensions")
+#!/usr/bin/env python3
+
+import math
 import inkex
+from lxml import etree
+
 class Circle():
 	def __init__(self,_c,_r):
 		self.radius=_r
 		self.center=_c
+        
 	def __str__(self):
 		return "Circle: center:"+str(self.center)+" radius:"+str(self.radius)+"\n"
+        
 	def __repr__(self):
 		return "Circle: center"+str(self.center)+" radius:"+str(self.radius)+"\n"
+        
 	def isHit(p):
 		distance=(center-p).length()
 		if(distance<radius):
 			return True
 		return False
-	#static関数
+
 	@classmethod
 	def toSVGObject(cls,x,y,r,color,strokewidth):
 		att={"cx":str(x),"cy":str(y),"r":str(r),"fill":color,"stroke-width":str(strokewidth)}
-		return inkex.etree.Element("circle",att)
+		return etree.Element("circle",att)
