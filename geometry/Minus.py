7b0da22dcd4ae3fda9b9bee1c201fc231082cbe0
Mario Voigt <opensource@leyghis.de>
2020-08-15 12:46:16 +0200

    Update Minus.py

--- geometry/Minus.py
+++ geometry/Minus.py
@@ -1,8 +1,9 @@
-#!/usr/bin/env python
-#coding: UTF-8
-import sys,os,math
-sys.path.append("/Applications/Inkscape.app/Contents/Resources/extensions")
+#!/usr/bin/env python3
+
+import math
 import inkex
+from lxml import etree
+
 class Minus():
 	def __init__(self,_x,_y,_r):
 		self.x=_x
@@ -14,15 +15,15 @@ class Minus():
 		if(distance<radius):
 			return True
 		return False
-	#static関数
+
 	def appendToSVG(self,color,svg):
 		attbackcicle={"cx":str(self.x),"cy":str(self.y),"r":str(self.r),"fill":"white",
 		"stroke-width":"0"}
 		attcicle={"cx":str(self.x),"cy":str(self.y),"r":str(self.r),"fill":color,"fill-opacity":"0.6",
 		"stroke-width":str(max(1,self.r/4)),"stroke":color}
-		#横線
+
 		atthline={"x1":str(self.x-self.r),"y1":str(self.y),
 		"x2":str(self.x+self.r),"y2":str(self.y),"stroke":color}
-		svg.append(inkex.etree.Element("circle",attbackcicle))
-		svg.append(inkex.etree.Element("circle",attcicle))
-		svg.append(inkex.etree.Element("line",atthline))
+		svg.append(etree.Element("circle",attbackcicle))
+		svg.append(etree.Element("circle",attcicle))
+		svg.append(etree.Element("line",atthline))
