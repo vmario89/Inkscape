f634b553affea13a2fe99b2ae649581280383f43
Mario Voigt <opensource@leyghis.de>
2020-08-15 12:46:26 +0200

    Update Plus.py

--- geometry/Plus.py
+++ geometry/Plus.py
@@ -1,8 +1,9 @@
-#!/usr/bin/env python
-#coding: UTF-8
-import sys,os,math
-sys.path.append("/Applications/Inkscape.app/Contents/Resources/extensions")
+#!/usr/bin/env python3
+
+import math
 import inkex
+from lxml import etree
+
 class Plus():
 	def __init__(self,_x,_y,_r):
 		self.x=_x
@@ -14,20 +15,20 @@ class Plus():
 		if(distance<radius):
 			return True
 		return False
-	#static関数
+
 	def appendToSVG(self,color,svg):
 		attbackcicle={"cx":str(self.x),"cy":str(self.y),"r":str(self.r),"fill":"white",
 		"stroke-width":"0"}
 		attcicle={"cx":str(self.x),"cy":str(self.y),"r":str(self.r),"fill":color,"fill-opacity":"0.6",
 		"stroke-width":str(max(1,self.r/4)),"stroke":color}
-		#横線
+		#horizontal line
 		atthline={"x1":str(self.x-self.r),"y1":str(self.y),
 		"x2":str(self.x+self.r),"y2":str(self.y),"stroke":color}
-		#縦線
+		#Vertical line
 		attvline={"x1":str(self.x),"y1":str(self.y-self.r),
 		"x2":str(self.x),"y2":str(self.y+self.r),"stroke":color}
-		#縦線
-		svg.append(inkex.etree.Element("circle",attbackcicle))
-		svg.append(inkex.etree.Element("circle",attcicle))
-		svg.append(inkex.etree.Element("line",atthline))
-		svg.append(inkex.etree.Element("line",attvline))
+		#Vertical line
+		svg.append(etree.Element("circle",attbackcicle))
+		svg.append(etree.Element("circle",attcicle))
+		svg.append(etree.Element("line",atthline))
+		svg.append(etree.Element("line",attvline))
